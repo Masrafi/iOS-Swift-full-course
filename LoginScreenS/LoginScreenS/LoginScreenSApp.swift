538ff72fcff87d15e01e7b16cf094d47556ebb5a
Masrafi Anam <42741317+Masrafi@users.noreply.github.com>
2023-02-14 16:42:55 +0600

    Add files via upload

---
+++ LoginScreenS/LoginScreenS/LoginScreenSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenSApp.swift
+//  LoginScreenS
+//
+//  Created by user on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
