40c33e01f0186a9a836afcb7f0c9e4c33818002e
Masrafi Anam <42741317+Masrafi@users.noreply.github.com>
2023-02-16 14:41:38 +0600

    Add files via upload

---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var myAge : Int = 32
+
+myAge = 33
+
+print(myAge)
+
+let myName : String = "Masrafi"
+var mySurename : String = "Anam"
+
+let myAgeInTenYears = myAge + 10
+let myFullName = myName + " Anam"
+let myFullNameAgain = "\(myName) \(mySurename)"
+let myDetails = "\(myName), \(myAge)."
+
+let wholeNumbers : Int = 12
+
+let text : String = "abc"
+
+let bool : Bool = true
+
+let floatingPointNumber : Float = 1.3
+
+let doubleNumber: Double = 3.123432
